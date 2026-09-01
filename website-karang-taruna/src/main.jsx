58c841540d9f54e000f692fba121362501575eb1
shn3vnn <evanchanel42@gmail.com>
2026-09-01 16:20:25 +0700

    Fix main.jsx entry and clean up unused admin import

--- website-karang-taruna/src/main.jsx
+++ website-karang-taruna/src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import Admin from './Admin.jsx' // pastikan nama file admin kamu sesuai
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
